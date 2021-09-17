a5ad97c53bae4eb31ba978dd0334ac8a994355fb
nanofaroque <faroque.ae@gmail.com>
2021-09-17 10:46:50 -0500

    add some caching for rod cutting problem

--- src/com/algorithms/clrs/chapter_15/RodCuttingRecursive.java
+++ src/com/algorithms/clrs/chapter_15/RodCuttingRecursive.java
@@ -1,18 +1,23 @@
 package com.algorithms.clrs.chapter_15;
 
+import java.util.HashMap;
+
 public class RodCuttingRecursive {
+    static HashMap<Integer,Integer> cache=new HashMap<>();
     public static void main(String[] args) {
-        int p[] ={1,5,8,9,10,17,17,20,24,30};
-        int n=10;
+        int p[] ={1,5,8,9,10,17,17,20,24,30,1,5,8,9,10,17,17,20,24,30,1,5,8,9,10,17,17,20,24,30,1,5,8,9,10,17,17,20,24,30};
+        int n=40;
         int val= maxProfit(p,n);
         System.out.println(val);
     }
     private static int maxProfit(int[] p, int n) {
         if(n==0) return 0;
+        if(cache.containsKey(n)) return cache.get(n);
         int profit=0;
         for(int i=1;i<=n;i++){
             profit=Math.max(profit,p[i-1]+maxProfit(p,n-i));
             System.out.println(profit);
+            cache.put(n,profit);
         }
         return profit;
     }
