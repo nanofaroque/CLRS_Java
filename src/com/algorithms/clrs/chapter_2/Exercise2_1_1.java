f83bb707c0381db15a0da80f3d3b414350802b94
nanofaroque <faroque.ae@gmail.com>
2021-09-23 23:38:24 -0500

    insertion sort

---
+++ src/com/algorithms/clrs/chapter_2/Exercise2_1_1.java
@@ -0,0 +1,22 @@
+package com.algorithms.clrs.chapter_2;
+
+public class Exercise2_1_1 {
+    public static void main(String[] args) {
+        int[] in = new int[]{31, 41, 59, 26, 41, 58};
+        sort(in);
+        for (int i : in)
+            System.out.println(i);
+    }
+
+    private static void sort(int[] in) {
+        for (int start = 1; start < in.length; start++) {
+            int key = in[start];
+            int i = start - 1;
+            while (i >= 0 && in[i] > key) {
+                in[i + 1] = in[i];
+                i--;
+            }
+            in[i + 1] = key;
+        }
+    }
+}
