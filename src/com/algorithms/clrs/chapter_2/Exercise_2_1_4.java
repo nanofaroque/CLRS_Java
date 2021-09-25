b00f8fd471a18c2f57192a72f1ca45d1a6487293
nanofaroque <faroque.ae@gmail.com>
2021-09-24 22:26:27 -0500

    minor change

--- src/com/algorithms/clrs/chapter_2/Exercise_2_1_4.java
+++ src/com/algorithms/clrs/chapter_2/Exercise_2_1_4.java
@@ -1,2 +1,27 @@
-package com.algorithms.clrs.chapter_2;public class Exercise_2_1_4 {
+package com.algorithms.clrs.chapter_2;
+
+public class Exercise_2_1_4 {
+    public static void main(String[] args) {
+        int n=3;
+        int[] a=new int[]{1,0,1};
+        int[] b=new int[]{1,1,0};
+
+        int[] sum=new int[n+1];
+        int carry=0;
+        int i;
+        for (i = n-1; i >=0 ; i--) {
+            int s=carry+a[i]+b[i];
+            if(s>=2){
+                carry=1;
+                sum[i]=0;
+            }else {
+                sum[i+1]=s;
+                carry=0;
+            }
+        }
+        if(carry>0)
+            sum[0]=carry;
+        for(int d:sum)
+            System.out.println(d);
+    }
 }
